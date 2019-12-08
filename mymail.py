37647f7a473da7d0f16f730102a1bbc8ada5753b
mastronardif <mastronardif@gmail.com>
2019-12-08 16:39:29 -0500

    mymyail

--- mymail.py
+++ mymail.py
@@ -4,16 +4,14 @@ import os
 import sys
 
 #print(os.environ['HOME'])
-#print(os.environ['CLOUDAMQP_URL'])
-url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost/%2f')  #os.environ['CLOUDAMQP_URL'] || 
 
-params = pika.URLParameters(url)
-params.socket_timeout = 5
+print(os.environ['MG_DOMAIN'])
+print(os.environ['MG_API_KEY'])
+#url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost/%2f')  #os.environ['CLOUDAMQP_URL'] || print(url())
 
-connection = pika.BlockingConnection(params) #   
-channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+sys.exit(23)
+
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
