53ab988e6ec1bdec1ab34995152ff5c9d194c0fe
Mastronardi <Frank.Mastronardi@Chubb.com>
2019-12-08 16:30:39 -0500

    upfsyrf msilhun

--- mymail.py
+++ mymail.py
@@ -1,32 +1,26 @@
 #!/usr/bin/env python
-import pika
 import os
 import sys
+import requests
 
 #print(os.environ['HOME'])
 #print(os.environ['CLOUDAMQP_URL'])
-url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost/%2f')  #os.environ['CLOUDAMQP_URL'] || 
+#url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost/%2f')  #os.environ['CLOUDAMQP_URL'] || 
+Mg__api_key='key-0-rxwnpe9gllqe6odwxebn79vicgxf76'
+Mg__dontuse=1
 
-params = pika.URLParameters(url)
-params.socket_timeout = 5
-
-connection = pika.BlockingConnection(params) #   
-channel = connection.channel()
-
-channel.queue_declare(queue='hello')
-
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
-    print(properties)
-    print(body.decode())
+def send_simple_message():
+    return requests.post(
+        " https://api.mailgun.net/v3/joeschedule.mailgun.org/messages",
+        auth=("api", Mg__api_key),
+        data={"from": "'Excited User <mastronardif@netcarrier.com>",
+              "to": ["mastronardif@gmail.com"],
+              "subject": "Hello",
+              "text": "Testing some Mailgun awesomness!"})
 
 
-
-
-channel.basic_consume(
-    queue='hello', on_message_callback=callback, auto_ack=True)
-
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+print(' [*] Waiting for send_simple_message. To exit press CTRL+C')
+resp = send_simple_message()
+print (resp)
 
 print("THE END")
